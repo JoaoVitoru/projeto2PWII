22da3b9febb9fe5180f60f5b2e8d2e71e7405ba8
João Vitor dos Santos Nunes <joaovitornunesdev@gmail.com>
2026-03-31 09:24:30 -0300

    adiantandoOProjeto

---
+++ Projeto2/Repository/LivroRepository.cs
@@ -0,0 +1,38 @@
+﻿using ProjetoEmprestimoAspCore.Models;
+using ProjetoEmprestimoAspCore.Repository.Contract;
+
+namespace ProjetoEmprestimoAspCore.Repository
+{
+    public class LivroRepository : ILivroRepository 
+    {
+        private readonly string _conexaoMySQL;
+        public LivroRepository(IConfiguration conf) { }
+
+        public void Atualizar(Livro livro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Cadastrar(Livro livro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Livro ObterLivros(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Livro> ObterTodosLivros()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+    
+}
--- Projeto2/obj/Debug/net8.0/Projeto2.AssemblyInfo.cs
+++ Projeto2/obj/Debug/net8.0/Projeto2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projeto2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27cb0ef4da20bcd86b229c578f038214bd5afc59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projeto2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projeto2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
